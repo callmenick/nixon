40067dafe4d227fd51396bdc53c99471e19cd59e
Nick Salloum <nick.salloum@axiomzen.co>
2016-01-10 12:41:07 -0400

    improves server code flow

--- server.js
+++ server.js
@@ -16,17 +16,51 @@ const morgan = require('morgan');
 const token = process.env.API_TOKEN || '';
 const app = express();
 
-app.use(morgan('combined'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+/**
+ * @name init
+ * @description Initializes the express application.
+ */
+(function init() {
 
-const server = http.createServer(app).listen(process.env.PORT || 3000, (err) => {
-  if (err) {
-    log('error', err);
-    return;
+  const port = process.env.PORT || 3000;
+  const server = http.createServer(app);
+
+  app.use(morgan('combined'));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.set('port', port);
+
+  server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
+
+  /**
+   * @name onError
+   * @description On error callback if there was on server listen.
+   * @param {Object} error The error.
+   */
+  function onError(error) {
+    switch (error.code) {
+      case 'EACCES':
+        log('error', `Port ${port} requires elevated privileges`);
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        log('error', `Port ${port} is already in use`);
+        process.exit(1);
+        break;
+      default:
+        log('error', error);
+    }
   }
 
-  log('info', 'Server listening on port ' + server.address().port);
+  /**
+   * @name onListening
+   * @description On listening callback if server successfully listens.
+   */
+  function onListening() {
+    log('success', `Server listening on port ${port}`);
+    new Bot(token);
+  }
 
-  return new Bot(token);
-});
+})();
